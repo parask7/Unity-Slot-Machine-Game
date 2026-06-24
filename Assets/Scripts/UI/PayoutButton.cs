a1ff432973b2a08818c4a18d59892a2aa39f4d3a
Parask <paraskukreja367@gmail.com>
2026-06-24 15:20:09 +0530

    Added documentation comments to payout information system

--- Assets/Scripts/UI/PayoutButton.cs
+++ Assets/Scripts/UI/PayoutButton.cs
@@ -1,16 +1,25 @@
 using UnityEngine;
 
+/// <summary>
+/// Displays the slot machine payout table using the popup system.
+/// 
+/// This script is typically attached to a UI button that allows
+/// players to view symbol rewards and payout information.
+/// </summary>
 public class PayoutButton : MonoBehaviour
 {
+    /// <summary>
+    /// Shows the current payout table in the popup window.
+    /// </summary>
     public void ShowPayouts()
     {
         PopUp.Instance?.ShowMessage(
-    "PAYOUTS\n\n" +
-    "Cherry   : 200\n" +
-    "Bell     : 300\n" +
-    "BAR      : 500\n" +
-    "Seven    : 1000\n\n" +
-    "Any Pair : 100"
-);
+            "PAYOUTS\n\n" +
+            "Cherry   : 200\n" +
+            "Bell     : 300\n" +
+            "BAR      : 500\n" +
+            "Seven    : 1000\n\n" +
+            "Any Pair : 100"
+        );
     }
 }
